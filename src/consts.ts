d805fe7b3f258c134598706ccc9b2677cb4f0001
LuisTM7 <luismiguel.tm7@gmail.com>
2025-06-09 19:23:25 -0500

    agregando datos

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Centro JHEST Bienestar Integral";
+export const SITE_DESCRIPTION =
+  "Un staff de psicólogos/as y una teóloga, unidos por el compromiso de brindar un acompañamiento integral (biopsicosocioespiritual) al bienestar humano.";
